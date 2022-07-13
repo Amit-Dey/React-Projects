3e77d85755367e92f2b968e64a743a082d564ffd
Amit-Dey <amitdey9020@gmail.com>
2022-07-14 02:09:56 +0600

    Adding files

---
+++ Project-01/index.js
@@ -0,0 +1,26 @@
+function Navbar() {
+    return (
+            <h1>Hello</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+    </div>,
+    document.getElementById("nev")
+)
+
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul>
+        <li>My name is Amit.</li>
+        <li>I am a Competitive Programmer.</li>
+        <li>I love Mobile Gamming.</li>
+    </ul>,
+    document.getElementById("list")
+)
+
+
+
