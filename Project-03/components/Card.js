946100b35975006790757a61268acacc633cc567
Amit-Dey <amitdey9020@gmail.com>
2022-07-20 23:59:38 +0600

    Updating Project-03

--- Project-03/components/Card.js
+++ Project-03/components/Card.js
@@ -1,17 +1,17 @@
 export default function Card(props) {
     return (
         <div className="card">
-            <img className="card-img" src={props.img}></img>
-            <h4 className="tag">{props.status}</h4>
+            <img className="card-img" src={props.item.img}></img>
+            {props.openSpots == 0 &&  <h4 className="tag">{props.item.status}</h4>}
             <div className="review">
                 <div className="r-row">
                     <img className="star"></img>
-                    <p className="rating">{props.rating}</p>
-                    <p className="no-of-rating">({props.sells})</p>
-                    <p className="country">{props.country}</p>
+                    <p className="rating">{props.item.rating}</p>
+                    <p className="no-of-rating">({props.item.sells})</p>
+                    <p className="country">{props.item.country}</p>
                 </div>
-                <p className="p-review">{props.description}</p>
-                <p className="price"><span>From ${props.price}</span> / person</p>
+                <p className="p-review">{props.item.title}</p>
+                <p className="price"><span>From ${props.item.price}</span> / person</p>
             </div>
 
         </div>
--- Project-03/components/MainContent.js
+++ Project-03/components/MainContent.js
@@ -1,4 +1,85 @@
+
 export default function MainContent() {
+    const Data = [
+        {
+            id: 1,
+            img: "./images/image_1.png",
+            title: "Life lessons with Katie Zaferes",
+            status: "SOLD OUT",
+            rating: 5.0,
+            sells: 6,
+            country: "USA",
+            price: 136,
+            openSpots:0,
+        },
+        {
+            id: 2,
+            img: "./images/image_2.png",
+            title: "Learn wedding photography",
+            status: "ONLINE",
+            rating: 5.0,
+            sells: 30,
+            country: "USA",
+            price: 125,
+            openSpots:0,
+            
+        },
+        {
+            id: 3,
+            img: "./images/image_3.png",
+            title: "Group Mountain Biking",
+            status: "ONLINE",
+            rating: 4.8,
+            sells: 2,
+            country: "USA",
+            price: 50,
+            openSpots:1,
+        },
+        {
+            id: 1,
+            img: "./images/image_1.png",
+            title: "Life lessons with Katie Zaferes",
+            status: "SOLD OUT",
+            rating: 5.0,
+            sells: 6,
+            country: "USA",
+            price: 136,
+            openSpots:0,
+        },
+        {
+            id: 2,
+            img: "./images/image_2.png",
+            title: "Learn wedding photography",
+            status: "ONLINE",
+            rating: 5.0,
+            sells: 30,
+            country: "USA",
+            price: 125,
+            openSpots:0,
+        },
+        {
+            id: 3,
+            img: "./images/image_3.png",
+            title: "Group Mountain Biking",
+            status: "ONLINE",
+            rating: 4.8,
+            sells: 2,
+            country: "USA",
+            price: 50,
+            openSpots:1,
+        },
+        
+        
+    ]
+    const card = Data.map(item=>{
+        return(
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })
+
     return (
         <div className="main">
             <div className="hero-section">
@@ -8,34 +89,7 @@ export default function MainContent() {
                 </p>
             </div>
             <div className="down">
-                <Card
-                    img="./images/image_1.png"
-                    status="SOLD OUT"
-                    rating="5.0"
-                    sells="6"
-                    country="USA"
-                    description="Life lessons with Katie Zaferes"
-                    price="136"
-
-                />
-                <Card
-                    img="./images/image_2.png"
-                    status="ONLINE"
-                    rating="5.0"
-                    sells="30"
-                    country="USA"
-                    description="Learn wedding photography"
-                    price="125"
-                />
-                <Card
-                    img="./images/image_3.png"
-                    status="ONLINE"
-                    rating="4.8"
-                    sells="2"
-                    country="USA"
-                    description="Group Mountain Biking"
-                    price="50"
-                />
+                {card}
                 
             </div>
         </div>
