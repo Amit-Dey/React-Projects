2e904f5bfe039d7f65fc4616d6b84abee875794c
Amit-Dey <amitdey9020@gmail.com>
2022-07-26 00:55:11 +0600

    updating project 05

--- Project-05/components/MainContent.js
+++ Project-05/components/MainContent.js
@@ -1,18 +1,21 @@
 export default function MainContent() {
+    function handelOnClick(){
+        console.log("I was clicked")
+    }
     return (
         <div className="main">
             <div className="input">
-                <input></input>
-                <input></input>
+                <input className="left_input" type="text" value="Top heading"></input>
+                <input className="right_input" type="text" value="Bottom heading"></input>
             </div>
-            <button>
+            <button onClick={handelOnClick}>
                 <p>Get a new meme image</p>
-                <img src=""></img>
+                <img src="./images/image_icon.png"></img>
             </button>
-            <div>
-                <img src=""></img>
-                <h1></h1>
-                <h1></h1>
+            <div className="content">
+                <img src="./images/content.png"></img>
+                <h1 className="top_heading">First heading</h1>
+                <h1 className="bottom_heading">Second heading</h1>
             </div>
         </div>
     )
